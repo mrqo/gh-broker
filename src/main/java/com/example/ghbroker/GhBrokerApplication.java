1a8826b56c4a4d271ee40b8dee9f0889367225ed
Marek Iwaniuk <miwan@softserveinc.com>
2021-05-10 12:06:56 +0200

    Init commit

---
+++ src/main/java/com/example/ghbroker/GhBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ghbroker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhBrokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GhBrokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ghbroker/GhBrokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ghbroker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhBrokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
