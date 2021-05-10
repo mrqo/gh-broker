71d011168307e0d58dc51a3634cab2becc1d27de
Marek Iwaniuk <elmarcopl@gmail.com>
2021-05-10 14:30:11 +0200

    REF: Updated repo model

--- src/main/java/com/example/ghbroker/RepoModel.java
+++ src/main/java/com/example/ghbroker/RepoModel.java
@@ -1,5 +1,8 @@
 package com.example.ghbroker;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,10 +10,15 @@ import java.time.Instant;
 
 @Getter
 @Setter
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class RepoModel {
     private String fullName;
+
     private String description;
+
     private String cloneUrl;
+
     private int watchers;
-    private Instant createdTime;
+
+    private Instant createdAt;
 }
